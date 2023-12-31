e79919015cb2dc7eaaa3f65f6619414fd0548e37
Playerzplays <150276053+Playerzplays@users.noreply.github.com>
2023-12-31 21:55:17 +0800

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -2,20 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-/**
- *
- * @author User
- */
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Newbie
- */
 import java.util.ArrayList;
 import java.util.List;
 
