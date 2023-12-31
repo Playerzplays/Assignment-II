d4e4ef146049ffa8e4a65c7efeda777fb6f837d1
Playerzplays <150276053+Playerzplays@users.noreply.github.com>
2023-12-31 21:56:13 +0800

    Update Warrior.java

--- src/Warrior.java
+++ src/Warrior.java
@@ -2,11 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-/**
- *
- * @author Newbie
- */
 class Warrior extends Character {
     private String weaponType;
     private String armorType;
