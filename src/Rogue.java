d16addb08965a7ddcc98e29b6500a65898718ecc
Playerzplays <150276053+Playerzplays@users.noreply.github.com>
2023-12-31 21:55:57 +0800

    Update Rogue.java

--- src/Rogue.java
+++ src/Rogue.java
@@ -2,11 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-/**
- *
- * @author Newbie
- */
 class Rogue extends Character {
     private int stealthLevel;
     private String daggerType;
